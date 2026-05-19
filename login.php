39a6e0eea26be2943371283708be6b7fb98b36f6
Leen-Albooq <leenalbooq048@gmail.com>
2026-05-19 19:59:09 +0300

    Update login.php

--- login.php
+++ login.php
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
   <meta charset="UTF-8">
   <title>Login — YIC IT Support</title>
-  <link rel="stylesheet" href="/yic-it-support/assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
 </head>
 
